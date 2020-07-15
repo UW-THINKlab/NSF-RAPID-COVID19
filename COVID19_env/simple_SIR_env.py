068a850fbe989ce492ea92dabb40607a536076af
benbernhard1 <bbernhar@caltech.edu>
2020-07-15 18:54:26 -0500

    made code more organized

--- COVID19_env/simple_SIR_env.py
+++ COVID19_env/simple_SIR_env.py
@@ -110,7 +110,7 @@ class simple_SIR_env(gym.Env):
     # R <--> python conversions
     numpy2ri.activate() # automatic conversion of numpy objects to rpy2 objects
     robjects.r('''
-           source('simple_SIR_model.R')
+           source('../COVID19_env/simple_SIR_model.R')
     ''') # source all R functions in the specified file
     sir_r = robjects.globalenv['sir_func'] # get R model
 
